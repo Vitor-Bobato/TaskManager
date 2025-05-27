189dc1703394ec1a875753ce67d98a56ac8e9503
Vitor Bobato <bobatovitor987@gmail.com>
2025-05-27 15:27:17 -0300

    Edição de tarefas + FrontEnd 1.0

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
-Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/create', [TaskController::class, 'create'])-> name ('tasks.create');
 Route::post('/tasks', [TaskController::class, 'store'])-> name ('tasks.store');
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
