cb2b6cc4e6c2a963632518e7146f382fea42f094
Paulo Cesar Cardoso Domingues <paulocesarcrds4@gmail.com>
2025-05-28 10:42:45 -0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\RegisterController;
 
-Route::get('/', [TaskController::class, 'index'])-> name ('tasks.index');
-Route::get('/tasks/create', [TaskController::class, 'create'])-> name ('tasks.create');
-Route::post('/tasks', [TaskController::class, 'store'])-> name ('tasks.store');
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
 
 // Rotas de criação de usuário
 Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
