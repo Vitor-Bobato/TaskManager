36b6158b49299f84ffccafbcd60f70b07cea1e24
Paulo Cesar Cardoso Domingues <134000626+Pcgo24@users.noreply.github.com>
2025-06-10 15:54:49 -0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,33 +1,4 @@
 <?php
-//
-//namespace App\Http\Middleware;
-//
-//use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-//use Illuminate\Contracts\Foundation\Application;
-//use Illuminate\Contracts\Encryption\Encrypter;
-//
-//class VerifyCsrfToken extends Middleware
-//{
-//    protected $app;
-//
-//    protected $except = [
-//    ];
-//
-//    public function __construct(Application $app, Encrypter $encrypter)
-//    {
-//        $this->app = $app;
-//        $this->encrypter = $encrypter;
-//    }
-//
-//    protected function tokensMatch($request)
-//    {
-//        if (app()->environment('testing') || app()->environment('cypress'))
-//        {
-//            return true;
-//        }
-//        return parent::tokensMatch($request);
-//    }
-//}
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
